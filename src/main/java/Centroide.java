e03d9382f8d956ff1c4852ba227055ea83daaac8
charles chaclim <chaclimesteves@gmail.com>
2019-06-30 06:47:57 -0300

    nao sei o erro mais ta em loop

--- src/main/java/Centroide.java
+++ src/main/java/Centroide.java
@@ -1,4 +1,3 @@
-import mpi.Datatype;
 import mpi.MPI;
 
 import java.util.List;
@@ -39,29 +38,70 @@ class Centroide extends Elemento {
     }
 
     boolean recalculaAtributosPar(List<Elemento> elementos){
-        int eu= MPI.COMM_WORLD.Rank();
-        int tamanho= MPI.COMM_WORLD.Size();
-        Double temp= this.numDimensoes / (tamanho*1.0);
-        int numPorThread= (int) Math.ceil(temp);
-        int[] recebidos= new int[numPorThread];
-        //int[] finais= new int[this.numDimensoes];         Talvez seja necessario
-        MPI.COMM_WORLD.Scatter(this.atributos, 0, numPorThread, Datatype.Contiguous(numPorThread,MPI.INT), recebidos, 0, numPorThread, Datatype.Contiguous(numPorThread, MPI.INT), 0);
-        boolean mudado= false;
-        for (int i = 0; i < numPorThread; i++) {
+        int nrThread = MPI.COMM_WORLD.Rank();
+        int tamanho = MPI.COMM_WORLD.Size();
+        int[] numPorThread = new int[tamanho*2];
+        numPorThread[0] = 0;
+        numPorThread[1] = (int) Math.floor(elementos.size() / (tamanho*1.0));
+        for(int i=2;i<tamanho*2;i+=2){
+            numPorThread[i]=numPorThread[i-1];
+            if(i!=tamanho*2-2){
+                numPorThread[i+1] = numPorThread[i]+(int) Math.floor(elementos.size() / (tamanho*1.0));
+            }else{
+                numPorThread[i+1] = elementos.size();
+            }
+        }
+
+        int[] recebidos = new int[2];
+        MPI.COMM_WORLD.Scatter(numPorThread, 0, 1, MPI.INT, recebidos, 0, 2, MPI.INT, 0);
+
+        boolean[] mudado = {false};
+        int[] listaMudancas = new int[numDimensoes];
+        for (int i = 0; i < numDimensoes; i++) {
             int soma = 0;
             int num = 0;
-            for (Elemento elemento : elementos) {
-                if (elemento.getAssociado() == this) {
+
+            for(int k = recebidos[0]; k < recebidos[1]; k++) {
+                if (elementos.get(k).getAssociado() == this) {
                     num++;
-                    soma += elemento.getAtributo(i);
+                    soma += elementos.get(k).getAtributo(i);
                 }
             }
-            if (num > 0) {
-                if (setAtributo(i, soma / num)) {
-                    mudado = true;
+
+            if(nrThread==0){
+                int[] numA = {0};
+                int[] somaA = {0};
+                for(int op=1; op<tamanho; op++){
+                    MPI.COMM_WORLD.Recv(numA, 0,1,MPI.INT,op,0);
+                    MPI.COMM_WORLD.Recv(somaA, 0,1,MPI.INT,op,1);
+                    soma+=somaA[0];
+                    num+=numA[0];
+                }
+                if (num > 0) {
+                    int aux = soma / num;
+                    if (setAtributo(i, aux)) {
+                        listaMudancas[i] = aux;
+                        mudado[0] = true;
+                    }else{
+                        listaMudancas[i] = -1;
+                    }
+                }
+            }else{
+                int[] numA = {num};
+                int[] somaA = {soma};
+                MPI.COMM_WORLD.Send(numA, 0,1,MPI.INT,0,0);
+                MPI.COMM_WORLD.Send(somaA, 0,1,MPI.INT,0,1);
+            }
+        }
+        MPI.COMM_WORLD.Bcast(mudado,0,1,MPI.BOOLEAN,0);
+        MPI.COMM_WORLD.Bcast(listaMudancas,0,1,MPI.INT,0);
+        if(mudado[0] && nrThread!=0){
+            for(int l=0; l < atributos.length;l++){
+                if(listaMudancas[l]!=-1){
+                    atributos[l] = listaMudancas[l];
                 }
             }
         }
-        return mudado;
+        return mudado[0];
     }
 }
--- src/main/java/trabalho2PC.java
+++ src/main/java/trabalho2PC.java
@@ -1,3 +1,4 @@
+import mpi.Datatype;
 import mpi.MPI;
 
 import java.io.IOException;
@@ -11,13 +12,10 @@ import java.util.Scanner;
 
 public class trabalho2PC{
 
-    private volatile boolean para = false;
+    private static boolean para = false;
     private final static Charset ENCODING = StandardCharsets.UTF_8;
     private static List<Elemento> elementos = new ArrayList<>();
     private static List<Centroide> centroides = new ArrayList<>();
-    static int quantThreads;
-    static List<Integer> dimensoesParaThread = new ArrayList<>();
-    private static int numDimensoes;
 
     private static int[] centro(int num, Scanner scanner) {
         String linha = scanner.nextLine();
@@ -57,8 +55,7 @@ public class trabalho2PC{
         }
     }
 
-    static private void k_meansSeq() {
-        boolean para = false;
+    private void k_meansSeq() {
         int numC;
         while (!para) {
             for (Elemento elemento : elementos) {
@@ -77,41 +74,23 @@ public class trabalho2PC{
         }
     }
 
-    private void k_meansPar() throws InterruptedException{
-        int i, soma = 0;
-        List<Thread> threads = new ArrayList<>();
-        List<ArrayList<Elemento>> elemetoParaThread = new ArrayList<>();
-
-        for (i = 0; i < quantThreads; i++) {
-            elemetoParaThread.add(new ArrayList<>());
-        }
-
-        for (i = 0; i < quantThreads-1; i++) {
-            elemetoParaThread.get(i).addAll(elementos.subList(soma, soma + elementos.size() / quantThreads));
-            soma += elementos.size() / quantThreads;
-        }
-        elemetoParaThread.get(quantThreads-1).addAll(elementos.subList(soma, elementos.size()));
-
-        soma = 0;
-
-        for (i = 0; i < trabalho2PC.quantThreads - 1; i++) {
-            dimensoesParaThread.add(i, soma + numDimensoes / trabalho2PC.quantThreads);
-            soma += numDimensoes / trabalho2PC.quantThreads;
-        }
-        dimensoesParaThread.add(trabalho2PC.quantThreads - 1, numDimensoes);
-
-
-        for (i = 0; i < quantThreads; i++) {
-            threads.add(new Thread(elemetoParaThread.get(i)));
-            threads.get(i).start();
-        }
-
+    private static void k_meansPar(){
+        System.out.println("Entrando "+ MPI.COMM_WORLD.Rank());
+        int numC;
         while (!para) {
-            barrier.await();
-        }
-
-        for (Thread thread : threads) {
-            thread.join();
+            for (Elemento elemento : elementos) {
+                elemento.encontraCentroide(centroides);
+            }
+            numC = 0;
+            for (Centroide centroide : centroides) {
+                boolean mudado = centroide.recalculaAtributosPar(elementos);
+                if (!mudado) {
+                    numC++;
+                }
+            }
+            if (numC == 20) {
+                para = true;
+            }
         }
     }
 
@@ -121,59 +100,85 @@ public class trabalho2PC{
         int nrThread = MPI.COMM_WORLD.Rank();
         int quantThreads= MPI.COMM_WORLD.Size();
         long startTempo = System.currentTimeMillis();
+
+        /* Seria o correto mas tem q fazer bCast não sei faze isso com classes imagina um list class
         if(nrThread == 0) {
             switch (args[3]) {
                 case "161":
                     carregaElementos(elementos, 161);
                     carregaCentroide(centroides, 161);
-                    numDimensoes = 161;
                     break;
 
                 case "256":
                     carregaElementos(elementos, 256);
                     carregaCentroide(centroides, 256);
-                    numDimensoes = 256;
                     break;
 
                 case "1380":
                     carregaElementos(elementos, 1380);
                     carregaCentroide(centroides, 1380);
-                    numDimensoes = 1380;
                     break;
 
                 case "1601":
                     carregaElementos(elementos, 1601);
                     carregaCentroide(centroides, 1601);
-                    numDimensoes = 1601;
                     break;
 
                 default:
                     carregaElementos(elementos, 59);
                     carregaCentroide(centroides, 59);
-                    numDimensoes = 59;
                     break;
             }//System.out.println("FIM "+nrThread);
+            MPI.COMM_WORLD.Barrier();
         }else{
             //System.out.println("Entrando "+nrThread);
             MPI.COMM_WORLD.Barrier();
             //System.out.println("Saindo "+nrThread);
         }
+         */
+
+            switch (args[3]) {
+                case "161":
+                    carregaElementos(elementos, 161);
+                    carregaCentroide(centroides, 161);
+                    break;
+
+                case "256":
+                    carregaElementos(elementos, 256);
+                    carregaCentroide(centroides, 256);
+                    break;
+
+                case "1380":
+                    carregaElementos(elementos, 1380);
+                    carregaCentroide(centroides, 1380);
+                    break;
+
+                case "1601":
+                    carregaElementos(elementos, 1601);
+                    carregaCentroide(centroides, 1601);
+                    break;
+
+                default:
+                    carregaElementos(elementos, 59);
+                    carregaCentroide(centroides, 59);
+                    break;
+            }
 
         if (quantThreads > 1) {
-            trabalho2PC trabalho = null;
             if(nrThread == 0) {
                 System.out.println("Execução Paralela");
-                trabalho = new trabalho2PC();
+                MPI.COMM_WORLD.Barrier();
             }else{
                 MPI.COMM_WORLD.Barrier();
             }
-            trabalho.k_meansPar();
+            k_meansPar();
         } else {
             System.out.println("Execução Sequencial");
-            k_meansSeq();
+            trabalho2PC trabalho = new trabalho2PC();
+            trabalho.k_meansSeq();
         }
 
-        if(nrThread == 0) {
+        if(nrThread == 1) {
             int i = 0;
             for (Elemento elemento : elementos) {
 
